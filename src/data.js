fdb7619792e0afae600b305546c387c2a66eda1c
Akash-Kumar <kumarakakash1999@gmail.com>
2021-01-03 20:12:57 +0530

    Add all my files

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data={
+    products:[
+        {
+        id:'1',
+        name:'Laptop',
+        price:'1400',
+        image:'https://images-na.ssl-images-amazon.com/images/I/314rRH-BnJL.jpg',
+       },
+       {
+        id:'2',
+        name:'Camera',
+        price:'900',
+        image:'https://images-na.ssl-images-amazon.com/images/I/81WtQ64-SOL._SL1500_.jpg',
+       },
+       {
+        id:'3',
+        name:'Mobile',
+        price:'400',
+        image:'https://images-na.ssl-images-amazon.com/images/I/71i2XhHU3pL._SL1500_.jpg',
+       },
+       
+    ],
+};
+export default data;
